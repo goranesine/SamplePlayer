b1b15c7ea870f66615db7fa11add7d3e3d1590c1
goranesine <goranesine@gmail.com>
2019-10-07 12:46:44 +0200

    druga verzija-treba dodati samplove

--- lib/main.dart
+++ lib/main.dart
@@ -18,12 +18,28 @@ class _ExampleAppState extends State<ExampleApp> {
   double _opacity = 0;
   Color _backgroundColor;
   bool _isPlaying = false;
+  double _firstButtonOpacity = 1;
+  double _secondButtonOpacity = 1;
+  double _thirdButtonOpacity = 1;
+  double _fourthButtonOpacity = 1;
+  double _fifthButtonOpacity = 1;
+  double _sixButtonOpacity = 1;
 
   Stream<double> _endSample() async* {
     advancedPlayer.onPlayerStateChanged.listen((AudioPlayerState s) {
       s == AudioPlayerState.COMPLETED ? _opacity = 0 : _opacity = 1;
       s == AudioPlayerState.COMPLETED ? _isPlaying = false : _opacity = 0;
+      s == AudioPlayerState.COMPLETED ? resetButtonOpacity() : _opacity = 0;
     });
+  }
+
+  resetButtonOpacity() {
+    _firstButtonOpacity = 1;
+    _secondButtonOpacity = 1;
+    _thirdButtonOpacity = 1;
+    _fourthButtonOpacity = 1;
+    _fifthButtonOpacity = 1;
+    _sixButtonOpacity = 1;
   }
 
   Color randomizecolor() {
@@ -36,20 +52,32 @@ class _ExampleAppState extends State<ExampleApp> {
     return _backgroundColor;
   }
 
-  checkIfIsPlaying(String sampleName) {
-    print("is-----play$_isPlaying");
+  changeButtonOpacity(int id) {
+    id == 1 ? _firstButtonOpacity = 0 : _firstButtonOpacity = 1;
+
+    id == 2 ? _secondButtonOpacity = 0 : _secondButtonOpacity = 1;
+    id == 3 ? _thirdButtonOpacity = 0 : _thirdButtonOpacity = 1;
+
+    id == 4 ? _fourthButtonOpacity = 0 : _fourthButtonOpacity = 1;
+    id == 5 ? _fifthButtonOpacity = 0 : _fifthButtonOpacity = 1;
+
+    id == 6 ? _sixButtonOpacity = 0 : _sixButtonOpacity = 1;
+  }
+
+  checkIfIsPlaying(String sampleName, int id) {
+    print(id);
+
     _isPlaying == true ? _opacity = 1 : loadMusic(sampleName);
+    _isPlaying == true ? _opacity = 1 : changeButtonOpacity(id);
   }
 
   Future loadMusic(String sampleName) async {
-    //changeOpacity();
     advancedPlayer = await AudioCache(prefix: "music/").play("$sampleName");
 
     setState(() {
       _isPlaying = true;
       _opacity = 1;
     });
-    print("isPlajening je $_isPlaying");
   }
 
   @override
@@ -77,47 +105,65 @@ class _ExampleAppState extends State<ExampleApp> {
                     mainAxisSpacing: 30,
                     crossAxisCount: 2,
                     children: <Widget>[
-                      RaisedButton(
-                        onPressed: () => checkIfIsPlaying("bass.wav"),
-                        elevation: 50,
-                        splashColor: Colors.redAccent,
-                        shape: CircleBorder(),
-                        color: Colors.redAccent,
+                      Opacity(
+                        opacity: _firstButtonOpacity,
+                        child: RaisedButton(
+                          onPressed: () => checkIfIsPlaying("bass.wav", 1),
+                          elevation: 50,
+                          splashColor: Colors.redAccent,
+                          shape: CircleBorder(),
+                          color: Colors.redAccent,
+                        ),
                       ),
-                      RaisedButton(
-                        onPressed: () => checkIfIsPlaying("bass.wav"),
-                        elevation: 50,
-                        splashColor: Colors.redAccent,
-                        shape: CircleBorder(),
-                        color: Colors.blue,
+                      Opacity(
+                        opacity: _secondButtonOpacity,
+                        child: RaisedButton(
+                          onPressed: () => checkIfIsPlaying("bass.wav", 2),
+                          elevation: 50,
+                          splashColor: Colors.redAccent,
+                          shape: CircleBorder(),
+                          color: Colors.blue,
+                        ),
                       ),
-                      RaisedButton(
-                        onPressed: () => checkIfIsPlaying("bass.wav"),
-                        elevation: 50,
-                        splashColor: Colors.redAccent,
-                        shape: CircleBorder(),
-                        color: Colors.amberAccent,
+                      Opacity(
+                        opacity: _thirdButtonOpacity,
+                        child: RaisedButton(
+                          onPressed: () => checkIfIsPlaying("bass.wav", 3),
+                          elevation: 50,
+                          splashColor: Colors.redAccent,
+                          shape: CircleBorder(),
+                          color: Colors.amberAccent,
+                        ),
                       ),
-                      RaisedButton(
-                        onPressed: () => checkIfIsPlaying("bass.wav"),
-                        elevation: 50,
-                        splashColor: Colors.redAccent,
-                        shape: CircleBorder(),
-                        color: Colors.deepOrangeAccent,
+                      Opacity(
+                        opacity: _fourthButtonOpacity,
+                        child: RaisedButton(
+                          onPressed: () => checkIfIsPlaying("bass.wav", 4),
+                          elevation: 50,
+                          splashColor: Colors.redAccent,
+                          shape: CircleBorder(),
+                          color: Colors.deepOrangeAccent,
+                        ),
                       ),
-                      RaisedButton(
-                        onPressed: () => checkIfIsPlaying("bass.wav"),
-                        elevation: 50,
-                        splashColor: Colors.redAccent,
-                        shape: CircleBorder(),
-                        color: Colors.deepPurpleAccent,
+                      Opacity(
+                        opacity: _fifthButtonOpacity,
+                        child: RaisedButton(
+                          onPressed: () => checkIfIsPlaying("bass.wav", 5),
+                          elevation: 50,
+                          splashColor: Colors.redAccent,
+                          shape: CircleBorder(),
+                          color: Colors.deepPurpleAccent,
+                        ),
                       ),
-                      RaisedButton(
-                        onPressed: () => checkIfIsPlaying("bass.wav"),
-                        elevation: 50,
-                        splashColor: Colors.redAccent,
-                        shape: CircleBorder(),
-                        color: Colors.green,
+                      Opacity(
+                        opacity: _sixButtonOpacity,
+                        child: RaisedButton(
+                          onPressed: () => checkIfIsPlaying("bass.wav", 6),
+                          elevation: 50,
+                          splashColor: Colors.redAccent,
+                          shape: CircleBorder(),
+                          color: Colors.green,
+                        ),
                       ),
                     ],
                   ),
@@ -126,9 +172,9 @@ class _ExampleAppState extends State<ExampleApp> {
     ));
   }
 
-  void changeOpacity() {
-    setState(() {
-      _opacity = 1;
-    });
-  }
+// void changeOpacity() {
+// setState(() {
+// _opacity = 1;
+//});
+//}
 }
